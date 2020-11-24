19fdf31beff5e2a0283bb10b69d36f982c14a559
CathomeGit <cathome.daily.music@gmail.com>
2020-11-24 20:29:36 +0300

    133. Basic layout

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,8 +12,52 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Card(),
+                Card(),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Card(),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Card(),
+                Card(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Card extends StatelessWidget {
+  final Color color;
+  static const Color DEFAULT_COLOR = Color(0xFF1D1E33);
+
+  Card({this.color = DEFAULT_COLOR});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10),
+        ),
       ),
     );
   }
