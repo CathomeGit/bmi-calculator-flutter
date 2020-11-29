6d84ba60c85d7d1689e5b4a6af11fc3ce87a9d1b
CathomeGit <cathome.daily.music@gmail.com>
2020-11-29 18:20:16 +0300

    Implement long press on buttons

--- lib/round_icon_button.dart
+++ lib/round_icon_button.dart
@@ -1,23 +1,41 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 const kFillColor = Color(0xFF4C4F5E);
 const kBoxConstraints = BoxConstraints.tightFor(width: 56.0, height: 56.0);
 
-class RoundIconButton extends StatelessWidget {
+class RoundIconButton extends StatefulWidget {
   final IconData child;
   final Function onPressed;
 
   RoundIconButton({@required this.child, @required this.onPressed});
 
   @override
+  _RoundIconButtonState createState() => _RoundIconButtonState();
+}
+
+class _RoundIconButtonState extends State<RoundIconButton> {
+  Timer _timer;
+
+  @override
   Widget build(BuildContext context) {
-    return RawMaterialButton(
-      onPressed: onPressed,
-      child: Icon(child),
-      fillColor: kFillColor,
-      elevation: 0.0,
-      shape: CircleBorder(),
-      constraints: kBoxConstraints,
+    return GestureDetector(
+      onLongPress: () {
+        _timer = Timer.periodic(Duration(milliseconds: 100), (t) {
+          widget.onPressed?.call();
+        });
+      },
+      onLongPressUp: () {
+        _timer.cancel();
+      },
+      child: RawMaterialButton(
+        onPressed: widget.onPressed,
+        child: Icon(widget.child),
+        fillColor: kFillColor,
+        elevation: 0.0,
+        shape: CircleBorder(),
+        constraints: kBoxConstraints,
+      ),
     );
   }
 }
